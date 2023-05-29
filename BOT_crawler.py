246279fe96fc0b94d5ed9bb583993533e3d6ea64
Liuvenas <47796376+Lukas-Vasionis@users.noreply.github.com>
2023-05-29 21:19:24 +0300

    refactored the crawler.py

--- BOT_crawler.py
+++ BOT_crawler.py
@@ -7,37 +7,43 @@ pd.set_option('display.expand_frame_repr', True)
 
 tipas = 'butu-nuoma'
 miestas = 'vilniuje'
-price_max = 1000
-price_min = 300
+price_min : int = 300
+price_max : int = 1000
 
-current_url = crl.set_current_url(tipas, miestas, price_min, price_max)
-# current_url='https://www.aruodas.lt/butu-nuoma/vilniuje/puslapis/63/?FPriceMax=300&FPriceMax=1000'
+START_URL = crl.set_START_URL(tipas, miestas, price_min, price_max)
 
 driver = sc.get_driver()
-crl.start_bot(current_url, driver, cookie_path="./cookies/cookies.pkl")
-input('c?')
-'''
-Crawling through the pages has the following logic:
-The crawler parses through the pages by clicking page element in the ruller at the bottom of the current page.
-When the last page is clicked (let say pg 13), the next page will lead you back the 13.
-Therfore, we check     
-'''
-# next_url_generated = crl.get_next_page_url(current_url)
-# next_url_resulted = next_url_generated
-next_url_generated = None
-next_url_resulted = None
+crl.start_bot(START_URL, driver)
 
+'''
+From the given filter a START_URL is constructed. From here, we start the crawling process.
+In the contents START_URL we look for the number of the last page in the page ruller at the buttom of the page.
+The page number will be later stored into last_pg_no variable and used for "for" loop.
+Before the loop, an empty df is created for storing the urls of ads and additional data.
+Inside the loop:
+
+    * Since the START_URL is the same as the 1st page in the page ruller, we go straight to the scraping part
+    
+    * The rest of the pages are constructed, accessed and then scraped.
+    
+    * The scraped data is placed into df_url_links
+    
+The final df gets deduplication and is saved into csv
+    
+'''
 
 df_url_links = pd.DataFrame(columns=['url', 'date_crawled', 'first_search_url'])
 
-while next_url_resulted == next_url_generated:
-    current_url = crl.get_current_page_url(driver)
 
-    df_url_links = crl.get_current_url_links(df_url_links, current_url, driver)
+last_pg_no = crl.get_last_pg_no(driver)
+for pg_no in range(1, last_pg_no + 1):
 
-    next_url_generated = crl.get_next_page_url(current_url)
-    crl.go_to_next_page_url(next_url_generated, driver)
-    next_url_resulted = crl.get_current_page_url(driver)
+    if pg_no != 1:
+        pg_url=crl.construct_pg_url(START_URL, pg_no)
+        crl.go_to_pg_url(pg_url, driver)
+
+    print(f'Crawling over page {pg_no}')
+    df_url_links = crl.get_pg_url_links(df_url_links, START_URL, driver)
 
 driver.quit()
 df_url_links = df_url_links.drop_duplicates(subset='url')
--- aruodas_lib_2/crawler.py
+++ aruodas_lib_2/crawler.py
@@ -1,4 +1,5 @@
-import os.path
+import regex as re
+import bs4
 import time
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
@@ -10,79 +11,61 @@ import pickle
 import selenium.webdriver
 
 
-
-
-
-def start_bot(current_url, driver, cookie_path):
+def start_bot(current_url, driver):
     """
-    Opens the target url and checks weather the cookies are up-to-date (weather it needs to click the banner of data usage consent):
-
-    * first it checks weather the cookie file exists:
-
-        * if yes, it loads it to the driver
-
-    * Second, it checks weather a cookies are up-to-date by checking if banner pops out
-
-        * If yes - cookies are out-dated: clicks the banned, updates cookies and loads it to the driver
-
-        * If not, it clicks the banner and saves cookies
-
+    * Enters current_url and clicks the data consent banner
     :param current_url: URL to start with
-    :param cookie_path: cookies
     :param driver: webdriver object to open url
     :return: None
     """
 
-    def update_cookies(action, driver, cookie_path):
-        '''
-
-        :param action: Chooaw weather to write or read cookies
-        :param driver: webdriver object
-        :param cookie_path: path to cookies (to save or to read)
-        :return: none
-        '''
-        if action == 'write':
-            pickle.dump(driver.get_cookies(), open(cookie_path, "wb+"))
-            print('Writing cookies...')
-        elif action == 'read':
-            cookies = pickle.load(open(cookie_path, "rb"))
-            for cookie in cookies:
-                driver.add_cookie(cookie)
-            print('Loading cookies...')
-
     driver.get(current_url)
 
-    # If cookies exist - load it
-    if os.path.exists(cookie_path):
-        driver.get(current_url)
-        update_cookies(action='read', driver=driver, cookie_path=cookie_path)
-        driver.get(current_url)
-
-    # If banner pops out - click it, update cookies and load them
+    # Click the data consent banner
     consent_banner = driver.find_elements(By.ID, "onetrust-accept-btn-handler")
-    if consent_banner:
-        consent_banner[0].click()
-        time.sleep(1)
-        update_cookies(action='write', driver=driver, cookie_path=cookie_path)
-        update_cookies(action='read', driver=driver, cookie_path=cookie_path)
-        driver.get(current_url)
-        input("i?")
-    driver.get(current_url)
-
-    time.sleep(1)
+    consent_banner[0].click()
 
 
-def set_current_url(tipas, miestas, price_max, price_min=None):
-    if price_min == None:
+
+
+def set_START_URL(tipas, miestas, price_max, price_min=None):
+    """
+
+    :param tipas: Pardavimo tipas - nuoma ('butu-nuoma') ar pardavimas ('butai')
+    :param miestas: Buto vietovė - miestas - kilmininko linksniu (pvz 'vilniuje')
+    :param price_max: Viršutinis ieškomos kainos limitas
+    :param price_min: Apatinis ieškomos kainos limitas
+    :return: None
+    """
+
+    if price_min is None:
         current_url = f'https://www.aruodas.lt/{tipas}/{miestas}/?FPriceMax={price_max}'
-    elif price_max == None:
+    elif price_max is None:
         current_url = f'https://www.aruodas.lt/{tipas}/{miestas}/?FPriceMin={price_min}'
     else:
         current_url = f'https://www.aruodas.lt/{tipas}/{miestas}/?FPriceMax={price_max}&FPriceMax={price_min}'
     return current_url
 
 
-def get_current_url_links(df_url_ads, current_url, driver):
+def get_last_pg_no(driver):
+    last_pg_no = bs4.BeautifulSoup(driver.page_source, 'xml')
+    last_pg_no = last_pg_no.find_all(class_="pagination")[0].find_all("a")
+    last_pg_no = [x for x in last_pg_no if x.string != '»'][-1]
+    last_pg_no = re.sub(r"[\n\t\s]*", "", last_pg_no.string)
+    last_pg_no = int(last_pg_no)
+
+    return last_pg_no
+
+def construct_pg_url(current_url, current_pg_no):
+    div_url = current_url.rsplit('/', 1)
+
+    url_root = div_url[0]
+    url_end = div_url[1]
+
+    next_url = f'{url_root}/puslapis/{str(current_pg_no)}/{url_end}'
+    return next_url
+
+def get_pg_url_links(df_url_ads, current_url, driver):
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     soup = soup.find_all(class_="list-search-v2")[0]
     soup = soup.find_all('a', href=True)
@@ -99,25 +82,6 @@ def get_current_url_links(df_url_ads, current_url, driver):
     return df_url_ads
 
 
-def get_next_page_url(current_url):
-    if 'puslapis' not in current_url:
-        current_url = current_url.rsplit('/', 1)
-        next_url = current_url[0] + f'/puslapis/2/' + current_url[1]
-    else:
-        div_url = current_url.rsplit('/', 1)
-        url_root = div_url[0].rsplit('/', 1)[0]
-        num = div_url[0].rsplit('/', 1)[1].strip()
-        print(f'Page {num}')
-        url_end = div_url[1]
-        next_url = f'{url_root}/{int(num) + 1}/{url_end}'
-    return next_url
-
-
-def go_to_next_page_url(next_url, driver):
-    driver.get(next_url)
+def go_to_pg_url(pg_url, driver):
+    driver.get(pg_url)
     time.sleep(random.uniform(3, 4))
-
-
-def get_current_page_url(driver):
-    current_url = driver.current_url
-    return (current_url)
